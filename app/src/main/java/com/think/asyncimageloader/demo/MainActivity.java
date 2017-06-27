ca1ef019f1eb98b9016dff29cfe09540e1cdba27
borney <borneywpf@gmail.com>
2017-06-27 11:38:57 +0800

    D

---
+++ app/src/main/java/com/think/asyncimageloader/demo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.think.asyncimageloader.demo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getSupportFragmentManager().findFragmentByTag(TAG) == null) {
+            final FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(android.R.id.content, new ImageGridFragment(), TAG);
+            ft.commit();
+        }
+    }
+}
